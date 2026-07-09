e506eadc835327649e3c9e97a656fce3f09bdc86
maarkevolution <104934297+maarkevolution@users.noreply.github.com>
2026-07-09 14:52:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
+// ================================
 // Petro Bill Generator
-// Version 1.0
+// ================================
 
-const buttons = document.querySelectorAll(".grid button");
+// Random Bill Number
+function generateBillNumber() {
 
-buttons.forEach((button) => {
-    button.addEventListener("click", function () {
-        alert("You selected: " + this.innerText);
+    const months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
 
-        // अगले स्टेप में यहीं से Bill Generator खुलेगा।
-    });
-});
+    const month = months[new Date().getMonth()];
+
+    const random = Math.floor(10000 + Math.random() * 90000);
+
+    return month + "-" + random + "-ORGNL";
+
+}
+
+// Random Transaction ID
+function generateTransactionID(){
+
+    return Math.floor(1000000000000000 + Math.random()*9000000000000000);
+
+}
+
+// Fill values automatically
+window.onload = function(){
+
+    const bill = document.getElementById("billNo");
+    const transaction = document.getElementById("transactionId");
+
+    if(bill){
+        bill.value = generateBillNumber();
+    }
+
+    if(transaction){
+        transaction.value = generateTransactionID();
+    }
+
+}
